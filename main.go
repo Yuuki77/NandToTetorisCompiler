34fe04ec91e16965f9b680f5c6b767bf1b33a4c7
Yuki Osaki <yuki.osaki7@gamail.com>
2021-03-23 21:02:32 +0900

    temp

--- main.go
+++ main.go
@@ -3,33 +3,8 @@ package main
 import "github.com/yukiOsaki/nandToTetorisCompiler/src"
 
 func main() {
-	// fmt.Println("hello world")
-
 	fileName := "chapter10/Square/Main.jack"
 	src.New(fileName)
-	// test := "class Main { "
-	// ans := strings.SplitAfter(test, "class")
-	// pretty.Print(ans)
-
-	// fmt.Println("%v", ans)
-	// fmt.Println("%v", len(ans))
-
-	// s := "function void main() {"
-	// fields := []string{}
-
-	// last := 0
-	// for i, r := range s {
-	// 	if r == ' ' {
-	// 		pretty.Println("last %v", last)
-	// 		pretty.Println("i is %v", i)
-
-	// 		pretty.Println("append one %v", s[last:i])
-
-	// 		fields = append(fields, s[last:i])
-	// 		last = i + 1
-	// 	}
-	// }
-	// pretty.Println(fields)
 }
 
 // func useFileRead(fileName string) {
